1e05586f5837af118cfd0cb326c8b7c29545a0a7
ahmad anwar <7552088+AhmedAnwarHafez@users.noreply.github.com>
2024-05-02 07:31:42 +1200

    hide harpoon

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -1,4 +1,27 @@
-return {
-  'folke/which-key.nvim', opts = {}
-}
+return {             -- Useful plugin to show you pending keybinds.
+  "folke/which-key.nvim",
+  event = "VimEnter", -- Sets the loading event to 'VimEnter'
+  config = function() -- This is the function that runs, AFTER loading
+    require("which-key").setup()
 
+    -- Document existing key chains
+    require("which-key").register({
+      ["<leader>c"] = { name = "[C]ode", _ = "which_key_ignore" },
+      ["<leader>d"] = { name = "[D]ocument", _ = "which_key_ignore" },
+      ["<leader>r"] = { name = "[R]ename", _ = "which_key_ignore" },
+      ["<leader>s"] = { name = "[S]earch", _ = "which_key_ignore" },
+      ["<leader>w"] = { name = "[W]orkspace", _ = "which_key_ignore" },
+      ["<leader>t"] = { name = "[T]oggle", _ = "which_key_ignore" },
+      ["<leader>h"] = { name = "Git [H]unk", _ = "which_key_ignore" },
+      ["<leader>1"] = "which_key_ignore",
+      ["<leader>2"] = "which_key_ignore",
+      ["<leader>3"] = "which_key_ignore",
+      ["<leader>4"] = "which_key_ignore",
+      ["<leader>5"] = "which_key_ignore",
+    })
+    -- visual mode
+    require("which-key").register({
+      ["<leader>h"] = { "Git [H]unk" },
+    }, { mode = "v" })
+  end,
+}
