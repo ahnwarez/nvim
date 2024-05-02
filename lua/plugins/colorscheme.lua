c2cc01ae96a2cc8d1a6b09c3879f3b86dd4d280a
ahmad anwar <7552088+AhmedAnwarHafez@users.noreply.github.com>
2024-05-02 14:41:23 +1200

    override colors

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,36 +1,36 @@
 local function vscode_config()
-    local vscode = require("vscode")
-    vscode.setup({
-        -- Enable transparent background
-        transparent = true,
+	local vscode = require("vscode")
+	local colors = require("vscode.colors").get_colors()
 
-        -- Enable italic comment
-        italic_comments = true,
-        group_overrides = {
-            -- CursorLine = { underline = true },
-            WinBar = { bg = nil }
-        },
-    })
-    vscode.load()
-    local colors = require("vscode.colors")
-    vim.api.nvim_set_hl(
-        0,
-        "NeoTreeFileNameOpened",
-        {
-            bg = colors.vscSelection,
-            fg = colors.vscBack,
-            bold = true,
-            underline = true,
-            italic = true
-        }
-    )
+	vscode.setup({
+		-- Enable transparent background
+		transparent = true,
+
+		-- Enable italic comment
+		italic_comments = true,
+		group_overrides = {
+			-- CursorLine = { underline = true },
+			WinBar = { bg = nil },
+
+			["@keyword"] = { fg = colors.vscPink, bg = "NONE" },
+			["typescriptBraces"] = { fg = colors.vscYellow, bg = "NONE" },
+			["@punctuation.bracket"] = { fg = "#ffcc00", bg = "NONE" },
+		},
+	})
+	vscode.load()
+	local colors = require("vscode.colors")
+	vim.api.nvim_set_hl(0, "NeoTreeFileNameOpened", {
+		bg = colors.vscSelection,
+		fg = colors.vscBack,
+		bold = true,
+		underline = true,
+		italic = true,
+	})
 end
 
-local vscode = {
-    "Mofiqul/vscode.nvim", -- Vscode type theme
-    lazy = false,
-    priority = 1000,
-    config = vscode_config
+return {
+	"Mofiqul/vscode.nvim", -- Vscode type theme
+	lazy = false,
+	priority = 1000,
+	config = vscode_config,
 }
-
-return vscode
