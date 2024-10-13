49fd5db9a0da350e9b3fe04b8f2006ab19b1baab
ahmad anwar <7552088+AhmedAnwarHafez@users.noreply.github.com>
2024-10-13 05:05:46 +0300

    update

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,19 +1,20 @@
 return {
-  "nvimtools/none-ls.nvim",
-  config = function()
-    local null_ls = require("null-ls")
+	"nvimtools/none-ls.nvim",
+	config = function()
+		local null_ls = require("null-ls")
 
-    null_ls.setup({
-      sources = {
-        null_ls.builtins.formatting.stylua,
-        null_ls.builtins.formatting.prettier,
-        null_ls.builtins.formatting.markdownlint,
-        null_ls.builtins.diagnostics.markdownlint,
-      },
-    })
+		null_ls.setup({
+			sources = {
+				null_ls.builtins.formatting.stylua,
+				null_ls.builtins.formatting.prettier,
+				null_ls.builtins.diagnostics.csharpier,
+				null_ls.builtins.formatting.markdownlint,
+				null_ls.builtins.diagnostics.markdownlint,
+			},
+		})
 
-    -- format first then save
-    vim.keymap.set("n", "<leader>fs", ":lua vim.lsp.buf.format()<ENTER>:w<CR>", { desc = "[F]ile [S]ave" })
-    vim.keymap.set("n", "<leader>cf", ":lua vim.lsp.buf.format()<ENTER>", { desc = "[C]ode [F]ormat" })
-  end,
+		-- format first then save
+		vim.keymap.set("n", "<leader>fs", ":lua vim.lsp.buf.format()<ENTER>:w<CR>", { desc = "[F]ile [S]ave" })
+		vim.keymap.set("n", "<leader>cf", ":lua vim.lsp.buf.format()<ENTER>", { desc = "[C]ode [F]ormat" })
+	end,
 }
